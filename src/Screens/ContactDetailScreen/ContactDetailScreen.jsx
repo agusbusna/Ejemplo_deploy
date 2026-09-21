e18eb47fd3fb6586ceb52e6fa2ce899d517f2b69
agustin.busna <agustinbusna@gmail.com>
2026-09-21 10:09:04 -0300

    arregle el Link to home

--- src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
+++ src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
@@ -67,7 +67,7 @@ export default function ContactDetailScreen() {
         <div className='body-screen-detail'>
             <Sidebar/>
             <div className='contact-detail-screen'>
-                <Link to="/home" className="btn-volver">
+                <Link to="/" className="btn-volver">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <path d="M19 12H5M12 19l-7-7 7-7" />
                     </svg>
